4beb57d65342ebff45dcd35e4d0c909e157b2e28
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-10-13 22:33:24 -0500

    Modelado de Cliente y Login
    Finalice el proceso para manejar datos por parte de las tablas login y cliente del backend

--- Backend/Store-B.0/src/main/java/D/S/Store/B0/repositories/LoginRepositories.java
+++ Backend/Store-B.0/src/main/java/D/S/Store/B0/repositories/LoginRepositories.java
@@ -1,5 +1,11 @@
 package D.S.Store.B0.repositories;
 
-public class LoginRepositories {
+import org.springframework.data.jpa.repository.JpaRepository;
+import D.S.Store.B0.models.Login;
+
+public  interface LoginRepositories extends JpaRepository<Login, Long> {
+    //Ese repositorio permite realizar operaciones CRUD sobre la entidad Login
     
-}
+    // Método para encontrar un Login por nombre de usuario y contraseña
+    Login findByUsuarioLoginAndPasswordLogin (String usuarioLogin, String passwordLogin);
+};
