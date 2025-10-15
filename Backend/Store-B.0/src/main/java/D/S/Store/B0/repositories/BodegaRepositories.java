6363186ff193d120319605e5b99a41749b005f3d
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-10-14 19:27:24 -0500

    Culminación de CRUD Y el cros
    agregue la logica para el informe y bodega también configure el cors para conectar el back con el front

--- Backend/Store-B.0/src/main/java/D/S/Store/B0/repositories/BodegaRepositories.java
+++ Backend/Store-B.0/src/main/java/D/S/Store/B0/repositories/BodegaRepositories.java
@@ -1,5 +1,8 @@
 package D.S.Store.B0.repositories;
 
-public class BodegaRepositories {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import D.S.Store.B0.models.Bodega;
+
+public interface BodegaRepositories extends JpaRepository <Bodega, Long> {
+    //Ese repositorio permite realizar operaciones CRUD sobre la entidad Bodega
+};
