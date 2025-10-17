92076a30e608b3253c2ead6aff6b8155e182b8a8
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-10-17 00:09:07 -0500

    Creacion base del login
    Cree el login básico, falta las mejoras y los implementar según el cargo y un poco de css

--- Frontend/store_B.O/src/App.jsx
+++ Frontend/store_B.O/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Principal from './components/componentsPrincipal/Principal.jsx';
+import InicioSesionForm from './components/componentsLogin/InicioSesionForm.jsx';
+import Logins from './components/componentsLogin/Logins.jsx';
 
 function App() {
 
   return (
-    <>
-    </>
+  // Configuración de las rutas de la aplicación
+  // Se utiliza BrowserRouter para manejar el enrutamiento
+  // Se definen las rutas y los componentes que se renderizarán para cada una
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<InicioSesionForm />} />
+        <Route path="/Logins" element={<Logins />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
